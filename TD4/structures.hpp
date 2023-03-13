1f2afa08e05524354d0aba78bff8f27e3a020c4c
Ali Gabr <aligabr03@gmail.com>
2023-03-13 12:00:57 -0400

    init tp4

---
+++ TD4/structures.hpp
@@ -0,0 +1,291 @@
+/*
+nom: structures.hpp
+description: Contient les definitions des structures et de la classe ListeFilms
+auteurs: Rayane Othmani (2126485) et Ali Gabr (2128904)
+date: 12 fevrier 2023
+*/
+
+#include <iostream>
+#include <memory>
+#include <fstream>
+#include "cppitertools/range.hpp"
+#include "gsl/span"
+
+using namespace iter;
+using namespace std;
+using namespace gsl;
+
+typedef uint16_t UInt16;
+
+UInt16 lireUint16(istream& fichier);
+
+struct Film;
+struct Acteur;
+class ListeFilms;
+
+Film* lireFilm(istream& fichier, ListeFilms& listeFilms);
+
+template <typename T>
+class Liste
+{
+public:
+	int capacite, nElements;
+	unique_ptr<shared_ptr<T>[]> elements;
+	Liste()
+	{
+		capacite = 0;
+		nElements = 0;
+		elements = make_unique<shared_ptr<T>[]>(0);
+	}
+	Liste(const Liste& liste)
+	{
+		capacite = liste.capacite;
+		nElements = liste.nElements;
+		elements = make_unique<shared_ptr<T>[]>(capacite);
+		for (int i = 0; i < nElements; i++)
+		{
+			elements[i] = liste.elements[i];
+		}
+	}
+	Liste(unsigned int cap, int n)
+	{
+		capacite = cap;
+		nElements = n;
+		elements = make_unique<shared_ptr<T>[]>(n);
+	}
+	friend ostream& operator<<(ostream& os, const T& elementaAfficher);
+	Liste<T> operator=(const Liste<T>& liste)
+	{
+		return liste;
+	}
+
+	void ajouterElement(shared_ptr<T> elementaAjouter)
+	{
+		if (capacite == nElements)
+		{
+			int nouvelleCapacite = max(1, 2 * capacite);
+			unique_ptr<shared_ptr<T>[]> nouvelleListe = make_unique<shared_ptr<T>[]>(nouvelleCapacite);
+			for (int i : range(nElements))
+			{
+				nouvelleListe[i] = elements[i];
+			}
+			elements = move(nouvelleListe);
+			capacite = nouvelleCapacite;
+		}
+		elements[nElements] = elementaAjouter;
+		nElements++;
+	}
+
+	shared_ptr<T>& operator[](const int& index)
+	{
+		return elements[index];
+	}
+};
+
+using ListeActeurs = Liste<Acteur>;
+
+class Item {
+public:
+	Item(){}
+	string getTitre() {
+		return titre;
+	}
+	friend Film* lireFilm(istream& fichier, ListeFilms& listeFilms);
+	friend ostream& operator<<(ostream& os, const Film& film);
+
+
+private:
+	string titre = "";
+	int anneeSortie = 0;
+};
+
+class Film : public Item{
+
+	Film()
+	{
+		realisateur = "";
+		recette = 0;
+	}
+	Film(unsigned int capacite, int nElements, string rea, int rec)
+	{
+		acteurs = ListeActeurs(capacite, nElements);
+		realisateur = rea;
+		recette = rec;
+	}
+
+	void detruireFilm()
+	{
+		for (int i : range(this->acteurs.nElements))
+		{
+			shared_ptr<Acteur> acteur = this->acteurs.elements[i];
+		}
+		delete this;
+	}
+
+	friend ostream& operator<<(ostream& os, const Film& film);
+
+	friend shared_ptr<Acteur> trouverActeur(const string& nomActeur);
+	friend Film* lireFilm(istream& fichier, ListeFilms& listeFilms);
+
+private:
+	std::string realisateur;
+	int recette;
+	ListeActeurs acteurs;
+	friend class ListeFilms;
+};
+
+class Livre : public Item {
+private:
+	string auteur;
+	int copiesVendues, nbPages;
+};
+
+struct Acteur
+{
+	std::string nom;
+	int anneeNaissance;
+	char sexe;
+	// ListeFilms joueDans;
+};
+
+class ListeFilms
+{
+public:
+	ListeFilms()
+	{ // Constructeur par defaut
+		capacite_ = 0;
+		nElements_ = 0;
+		elements_ = {};
+	}
+
+	ListeFilms(string nomFichier)
+	{
+		ListeFilms temp = creerListe(nomFichier);
+		capacite_ = temp.capacite_;
+		nElements_ = temp.nElements_;
+		elements_ = temp.elements_;
+	}
+
+	int getnElements()
+	{
+		return nElements_;
+	}
+
+	Film** getElements()
+	{
+		return elements_;
+	}
+
+	ListeFilms creerListe(string nomFichier)
+	{
+
+		ifstream fichier(nomFichier, ios::binary);
+		fichier.exceptions(ios::failbit);
+
+		int nElements = lireUint16(fichier);
+
+		ListeFilms listeFilms;
+
+		for ([[maybe_unused]] int i : range(nElements))
+		{
+			Film* film = lireFilm(fichier, listeFilms);
+			listeFilms.ajouterFilm(film);
+		}
+		return listeFilms;
+	}
+
+	void ajouterFilm(Film* film)
+	{
+		if (capacite_ == nElements_)
+		{
+			int nouvelleCapacite = max(1, 2 * capacite_);
+			Film** nouvelleListe = new Film*[nouvelleCapacite];
+			for (int i : range(nElements_))
+			{
+				nouvelleListe[i] = elements_[i];
+			}
+			delete[] elements_;
+			elements_ = nouvelleListe;
+			capacite_ = nouvelleCapacite;
+		}
+		elements_[nElements_] = film;
+		nElements_++;
+	}
+
+	void enleverFilm(Film* filmAEnlever)
+	{
+		for (int i : range(nElements_))
+		{
+			if (elements_[i] == filmAEnlever)
+			{
+				if (i != nElements_ - 1)
+				{
+					elements_[i] = elements_[nElements_ - 1];
+					nElements_--;
+				}
+				else
+				{
+					nElements_--;
+				}
+			}
+		}
+	}
+
+	shared_ptr<Acteur> trouverActeur(const string& nomActeur)
+	{
+		for (int i : range(nElements_))
+		{
+			Film* film = elements_[i];
+			for (shared_ptr<Acteur>& acteur : span<shared_ptr<Acteur>>(film->acteurs.elements.get(), film->acteurs.nElements))
+			{
+				if (acteur->nom == nomActeur)
+					return acteur;
+			}
+		}
+
+		return nullptr;
+	}
+
+	void detruire()
+	{
+		for (int i : range(nElements_))
+		{
+			(*elements_[i]).detruireFilm(); ////////////////////////////////////////////////////////////////
+		}
+		delete[] elements_;
+	}
+
+	void afficher() const
+	{
+		static const string ligneDeSeparation = "\n\u007C\u007C\u007C\u007C\u007C\u007C\u007C\u007C\u007C\u007C\u007C\n";
+		cout << ligneDeSeparation;
+		for (Film* film : span<Film*>(elements_, nElements_))
+		{
+			cout << *film;
+			cout << ligneDeSeparation;
+		}
+	}
+
+	Film* operator[](const int& index)
+	{
+		return elements_[index];
+	}
+
+	template <typename T>
+	Film* afficherFilmParCritere(const T& critere)
+	{
+		for (int i : range(nElements_))
+		{
+			if (critere(elements_[i]))
+			{
+				return elements_[i];
+			}
+		}
+		return nullptr;
+	}
+
+
+private:
+	int capacite_, nElements_;
+	Film** elements_;
+};
---
+++ TD4/td4.cpp
@@ -0,0 +1,113 @@
+﻿/*
+nom: td2.cpp
+description: Contient les fonctions pour lire un fichier et pour gerer un objet de type Film ainsi que les appels de fonction
+auteurs: Rayane Othmani (2126485) et Ali Gabr (2128904)
+date: 1 mars 2023
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <limits>
+#include <algorithm>
+#include <memory>
+
+#include "cppitertools/range.hpp"
+#include "gsl/span"
+
+#include "structures.hpp"
+#include "bibliotheque_cours.hpp"
+#include "verification_allocation.hpp"
+#include "debogage_memoire.hpp"
+
+using namespace std;
+using namespace iter;
+using namespace gsl;
+
+typedef uint8_t UInt8;
+typedef uint16_t UInt16;
+
+#pragma region Fonctions de base pour lire le fichier binaire
+
+UInt8 lireUint8(istream& fichier)
+{
+	UInt8 valeur = 0;
+	fichier.read((char *)&valeur, sizeof(valeur));
+	return valeur;
+}
+UInt16 lireUint16(istream& fichier)
+{
+	UInt16 valeur = 0;
+	fichier.read((char *)&valeur, sizeof(valeur));
+	return valeur;
+}
+string lireString(istream& fichier)
+{
+	string texte;
+	texte.resize(lireUint16(fichier));
+	fichier.read((char *)&texte[0], streamsize(sizeof(texte[0])) * texte.length());
+	return texte;
+}
+
+#pragma endregion
+
+shared_ptr<Acteur> lireActeur(istream& fichier, ListeFilms& listeFilms)
+{
+	Acteur acteur = {};
+	acteur.nom = lireString(fichier);
+	acteur.anneeNaissance = lireUint16(fichier);
+	acteur.sexe = lireUint8(fichier);
+
+	shared_ptr<Acteur> acteurExistant = listeFilms.trouverActeur(acteur.nom);
+	if (acteurExistant != nullptr)
+	{
+		cout << "L'Acteur existe et son nom est le suivant : " << acteurExistant->nom << endl;
+		return acteurExistant;
+	}
+	cout << "Un nouvel acteur a été créé et son nom est : " << acteur.nom << endl;
+	acteurExistant = make_shared<Acteur>(Acteur(acteur));
+	return acteurExistant;
+}
+
+Film* lireFilm(istream& fichier, ListeFilms& listeFilms)
+{
+	Film film = {};
+	film.titre = lireString(fichier);
+	film.realisateur = lireString(fichier);
+	film.anneeSortie = lireUint16(fichier);
+	film.recette = lireUint16(fichier);
+	int nElements = lireUint8(fichier);
+
+	Film *filmAlloue = new Film(film);
+	for ([[maybe_unused]] int i : range(nElements))
+	{
+		filmAlloue->acteurs.ajouterElement(lireActeur(fichier, listeFilms));
+	}
+	return filmAlloue;
+}
+
+ostream& operator<<(ostream& os, const Acteur& acteur)
+{
+	os << "  " << acteur.nom << ", " << acteur.anneeNaissance << " " << acteur.sexe << endl;
+	return os;
+}
+
+ostream& operator<<(ostream& os, const Film& film)
+{
+	os << "Les acteurs qui jouent dans le film " << film.titre << " sont" << endl;
+	for (int i : range(film.acteurs.nElements))
+	{
+		os << *film.acteurs.elements[i];
+	}
+	return os;
+}
+
+int main()
+{
+	bibliotheque_cours::activerCouleursAnsi(); // Permet sous Windows les "ANSI escape code" pour changer de couleurs https://en.wikipedia.org/wiki/ANSI_escape_code ; les consoles Linux/Mac les supportent normalement par défaut.
+
+	static const string ligneDeSeparation = "\n\033[35m════════════════════════════════════════\033[0m\n";
+
+	ListeFilms liste("films.bin");
+	liste.detruire();
+}
