221b74a8e2ad31227aee124100f992986178feac
Ali Gabr <aligabr03@gmail.com>
2023-03-26 11:55:42 -0400

    2.1 build error

--- TD5/structures.hpp
+++ TD5/structures.hpp
@@ -37,6 +37,10 @@ class Liste
 public:
 	int capacite, nElements;
 	unique_ptr<shared_ptr<T>[]> elements;
+
+	unique_ptr<shared_ptr<T>>* begin() { return elements; }
+	unique_ptr<shared_ptr<T>>* end() { return elements + capacite; }
+
 	Liste()
 	{
 		capacite = 0;
@@ -101,10 +105,16 @@ class Item : public Affichable
 {
 public:
 	Item() {}
+	string titre = "";
+	int annee = 0;
+
+	bool compareItems(const Item& p1, const Item& p2) {
+		return p1.titre < p2.titre;
+	}
 
 	void lireFichier(ifstream& fichier)
 	{
-		fichier >> quoted(titre_) >> annee_;
+		fichier >> quoted(titre) >> annee;
 	}
 
 	friend class ListeFilms;
@@ -113,27 +123,30 @@ public:
 
 	void afficher() const override
 	{
-		cout << "Titre : " << titre_ << "\tAnnee: " << annee_ << endl;
+		cout << titre ;
 	}
-
-private:
-	string titre_ = "";
-	int annee_ = 0;
+	
 };
 
 class Film : virtual public Item
 {
 public:
+	string realisateur;
+	int recette;
+	ListeActeurs acteurs;
+
+
+
 	Film()
 	{
-		realisateur_ = "";
-		recette_ = 0;
+		realisateur = "";
+		recette = 0;
 	}
 	Film(unsigned int capacite, int nElements, string rea, int rec)
 	{
-		acteurs_ = ListeActeurs(capacite, nElements);
-		realisateur_ = rea;
-		recette_ = rec;
+		acteurs = ListeActeurs(capacite, nElements);
+		realisateur = rea;
+		recette = rec;
 	}
 
 	friend class ListeFilms;
@@ -143,24 +156,21 @@ public:
 	void afficher() const override
 	{
 		Item::afficher();
-		cout << "Combo :\n"
-			 << "  Realisateur : " << realisateur_ << "\n  Recette : " << recette_ << "M$" << endl;
-		cout << "Acteurs : " << endl;
-		for (shared_ptr<Acteur> &acteur : span<shared_ptr<Acteur>>(this->acteurs_.elements.get(), this->acteurs_.nElements))
-		{
-			cout << *acteur;
-		}
+		cout << ", par le réalisateur : " << realisateur << endl;
+		// cout << "Acteurs : " << endl;
+		// for (shared_ptr<Acteur> &acteur : span<shared_ptr<Acteur>>(this->acteurs.elements.get(), this->acteurs.nElements))
+		// {
+		// 	cout << *acteur;
+		// }
 	}
 
-private:
-	string realisateur_;
-	int recette_;
-	ListeActeurs acteurs_;
 };
 
 class Livre : virtual public Item
 {
 public:
+	string auteur;
+	int copiesVendues, nbDePages;
 	Livre(ifstream& fichier)
 	{
 		Item::lireFichier(fichier);
@@ -169,26 +179,20 @@ public:
 
 	void lireFichier(ifstream& fichier)
 	{
-		fichier >> quoted(auteur_) >> copiesVendues_ >> nbDePages_;
+		fichier >> quoted(auteur) >> copiesVendues >> nbDePages;
 	}
 
 	void afficher() const override
 	{
 		Item::afficher();
-		cout << "  Auteur : " << auteur_ << "\n  Copies Vendues : " << copiesVendues_ << "M"
-			 << "\tPages : " << nbDePages_ << endl;
+		cout << ", par l'auteur : " << auteur << endl;
 	}
 
 	void afficherInfo() const
 	{
-		cout << "Livre :\n"
-			 << "  Auteur : " << auteur_ << "\n  Copies Vendues : " << copiesVendues_ << "M"
-			 << "\tPages : " << nbDePages_ << endl;
+		cout << "Et de l'auteur : " << auteur << endl;
 	}
 
-private:
-	string auteur_;
-	int copiesVendues_, nbDePages_;
 };
 
 class FilmLivre : public Film, public Livre
@@ -213,6 +217,8 @@ struct Acteur
 class ListeFilms
 {
 public:
+	int capacite_, nElements_;
+	Film** elements_;
 	ListeFilms()
 	{ // Constructeur par defaut
 		capacite_ = 0;
@@ -298,7 +304,7 @@ public:
 		for (int i : range(nElements_))
 		{
 			Film *film = elements_[i];
-			for (shared_ptr<Acteur> &acteur : span<shared_ptr<Acteur>>(film->acteurs_.elements.get(), film->acteurs_.nElements))
+			for (shared_ptr<Acteur> &acteur : span<shared_ptr<Acteur>>(film->acteurs.elements.get(), film->acteurs.nElements))
 			{
 				if (acteur->nom == nomActeur)
 					return acteur;
@@ -347,6 +353,5 @@ public:
 	}
 
 private:
-	int capacite_, nElements_;
-	Film** elements_;
+
 };
